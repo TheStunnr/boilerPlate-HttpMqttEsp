60c5e22090862016fe95360bd8d7fb8cdaeb5bef
TheStunnr <thestunnr@gmail.com>
2017-12-27 16:07:07 +0800

    Pin control, using serial

--- hTest-HttpMqttEsp.ino
+++ hTest-HttpMqttEsp.ino
@@ -10,30 +10,52 @@ WiFiClient espClient;                             // Initiate WiFiClient Library
 
 const char* ssid = "ZTEH108N_3C01E";
 const char* password =  "X9425TE9";
-char serialValue = 0;
+int serialValue = 0;
+int serialValueConv = 0;
 int pinOut04 = D4;
+int pinOut03 = D3;
 
 void setup() {
+  pinMode(pinOut04, OUTPUT);
+  pinMode(pinOut03, OUTPUT); 
+  
   Serial.begin(115200);
   WiFi.begin(ssid, password);                     // Initialize WiFi Connection 
  
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
+      delay(500);
+      Serial.println("Connecting to WiFi..");
   }
+  
   Serial.println("Connected to the WiFi network");
  
 }
 
 void loop() {
    
-  if(WiFi.status()== WL_CONNECTED)  {            
+  if(WiFi.status() == WL_CONNECTED)  {            
     
-    digitalWrite(pinOut04, HIGH);                 // Power Indicator
-    serialValue = Serial.read();                  // Serial Input
+      /* Power and Internet Connection */
+      
+      digitalWrite(pinOut04, HIGH);                 
+      
+      /* Serial Input Tests */
+      
+      serialValue = Serial.read();                  // Serial Input   
+      serialValueConv = serialValue - 48;           // Temporary Conversion from ASCII           
+                
       Serial.print("From Serial: ");
-      Serial.println(serialValue);
- 
+      Serial.println(serialValueConv);
+              
+              if (serialValueConv == 1) {
+                  digitalWrite(pinOut03, HIGH);  
+                 
+              }
+              else if (serialValueConv == 2)  {
+                  digitalWrite(pinOut03, LOW);        
+              }
+
+                          
     delay(1000);        
   }
   
